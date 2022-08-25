5464eb120d6c6c80d57f8b6a6cb77b74aeb15555
21A91A6153 <97424603+21A91A6153@users.noreply.github.com>
2022-08-25 15:03:09 +0530

    import java.util.Scanner; class Multiplication {     public static void main(String args[])     {        Scanner sc=new Scanner(System.in);        int n,m,c=0;        n=sc.nextInt();        m=sc.nextInt();        System.out.print(n/m);     } }

---
+++ Division.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Multiplication
+{
+    public static void main(String args[])
+    {
+       Scanner sc=new Scanner(System.in);
+       int n,m,c=0;
+       n=sc.nextInt();
+       m=sc.nextInt();
+       System.out.print(n/m);
+    }
+}

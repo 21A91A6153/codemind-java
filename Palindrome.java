2279801a876966681176fe4926ce12aa4ade68a5
21A91A6153 <97424603+21A91A6153@users.noreply.github.com>
2022-09-01 20:04:15 +0530

    import java.util.Scanner; class Display {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,rev=0,temp,d;         a=sc.nextInt();         temp=a;         while(a!=0)         {             d=a%10;             a=a/10;             rev=rev*10+d;         }         if(rev==temp)         {             System.out.print("True");         }         else         {              System.out.print("False");         }     } }

---
+++ Palindrome.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Display
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,rev=0,temp,d;
+        a=sc.nextInt();
+        temp=a;
+        while(a!=0)
+        {
+            d=a%10;
+            a=a/10;
+            rev=rev*10+d;
+        }
+        if(rev==temp)
+        {
+            System.out.print("True");
+        }
+        else
+        {
+             System.out.print("False");
+        }
+    }
+}

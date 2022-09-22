066662632bb10f4bca93aaf431b7c2b84050c953
21A91A6153 <97424603+21A91A6153@users.noreply.github.com>
2022-09-22 21:40:04 +0530

    import java.util.Scanner; class Simple {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         double p,t,r,ci;         p=sc.nextDouble();         t=sc.nextDouble();         r=sc.nextDouble();         ci=p*(Math.pow(1+(t/100),r));         System.out.format("%.2f",ci);
        }
    }

---
+++ Compound_interest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Simple
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        double p,t,r,ci;
+        p=sc.nextDouble();
+        t=sc.nextDouble();
+        r=sc.nextDouble();
+        ci=p*(Math.pow(1+(t/100),r));
+        System.out.format("%.2f",ci);
+
+    }
+}

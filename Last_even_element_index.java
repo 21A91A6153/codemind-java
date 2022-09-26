435cfdca3770c4eb1ed382c9472d20490b2f0ce4
21A91A6153 <97424603+21A91A6153@users.noreply.github.com>
2022-09-26 23:03:18 +0530

    import java.util.Scanner; class Search {     public static void main(String args[])     {         int n,x[],i,m,c=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         //m=sc.nextInt();         for(i=(x.length-1);i>=0;i--)         {             if(x[i]%2==0)             {                 System.out.print(i);                 break;             }         }     } }

---
+++ Last_even_element_index.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Search
+{
+    public static void main(String args[])
+    {
+        int n,x[],i,m,c=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        //m=sc.nextInt();
+        for(i=(x.length-1);i>=0;i--)
+        {
+            if(x[i]%2==0)
+            {
+                System.out.print(i);
+                break;
+            }
+        }
+    }
+}

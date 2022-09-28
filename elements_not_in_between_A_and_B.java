6455fb7785314d366249a93781bc22290060021b
21A91A6153 <97424603+21A91A6153@users.noreply.github.com>
2022-09-28 23:28:34 +0530

    import java.util.Scanner; class Count {     public static void main(String args[])     {         int n,i,c=0,k=0,l=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int x[]=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         int m=sc.nextInt();         int a=sc.nextInt();         for(i=0;i<x.length;i++)         {             if(x[i]>=m && x[i]<=a)             {                 continue;             }             else             {                 c++;                 System.out.print(x[i]+" ");             }         }         if(c==0)         {             System.out.print("-1");         }     } }

---
+++ elements_not_in_between_A_and_B.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Count
+{
+    public static void main(String args[])
+    {
+        int n,i,c=0,k=0,l=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int x[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        int m=sc.nextInt();
+        int a=sc.nextInt();
+        for(i=0;i<x.length;i++)
+        {
+            if(x[i]>=m && x[i]<=a)
+            {
+                continue;
+            }
+            else
+            {
+                c++;
+                System.out.print(x[i]+" ");
+            }
+        }
+        if(c==0)
+        {
+            System.out.print("-1");
+        }
+    }
+}

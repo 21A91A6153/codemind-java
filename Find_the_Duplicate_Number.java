7b12ff21b840e8c339ff1ae3e246d400374a627d
21A91A6153 <97424603+21A91A6153@users.noreply.github.com>
2022-10-07 22:00:12 +0530

    import java.util.Scanner; class Product {     public static void main(String args[])     {         int n,i,mul=1,k=0,j;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int x[]=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(i=0;i<x.length;i++)         {             k=0;             for(j=0;j<x.length;j++)             {                 if(x[i]==x[j])                 {                     k++;                 }             }             if(k>1)             {                 System.out.print(x[i]+" ");                 x[i]=' ';             }         }
        }
    }

---
+++ Find_the_Duplicate_Number.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class Product
+{
+    public static void main(String args[])
+    {
+        int n,i,mul=1,k=0,j;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int x[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(i=0;i<x.length;i++)
+        {
+            k=0;
+            for(j=0;j<x.length;j++)
+            {
+                if(x[i]==x[j])
+                {
+                    k++;
+                }
+            }
+            if(k>1)
+            {
+                System.out.print(x[i]+" ");
+                x[i]=' ';
+            }
+        }
+        
+    }
+}

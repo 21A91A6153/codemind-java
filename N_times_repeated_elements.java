92984490fede50862b4685be13412c24d3673a5c
21A91A6153 <97424603+21A91A6153@users.noreply.github.com>
2022-10-07 21:16:50 +0530

    import java.util.Scanner; class Times {     public static void main(String args[])     {         int n,i,c=0,k,j,a=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int x[]=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         k=sc.nextInt();         for(i=0;i<x.length;i++)         {             c=0;             for(j=0;j<x.length;j++)             {                 if(x[i]==x[j])                 {                     c++;                 }             }             if(c==k)             {                 System.out.print(x[i]+" ");                 x[i]=' ';                 a++;             }         }         if(a==0)         {             System.out.print(-1);         }     } }

---
+++ N_times_repeated_elements.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class Times
+{
+    public static void main(String args[])
+    {
+        int n,i,c=0,k,j,a=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int x[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        k=sc.nextInt();
+        for(i=0;i<x.length;i++)
+        {
+            c=0;
+            for(j=0;j<x.length;j++)
+            {
+                if(x[i]==x[j])
+                {
+                    c++;
+                }
+            }
+            if(c==k)
+            {
+                System.out.print(x[i]+" ");
+                x[i]=' ';
+                a++;
+            }
+        }
+        if(a==0)
+        {
+            System.out.print(-1);
+        }
+    }
+}

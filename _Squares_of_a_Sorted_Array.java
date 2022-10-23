21ad67beff4d4af4302a4fd3dc3101e5ff8cb627
21A91A6153 <97424603+21A91A6153@users.noreply.github.com>
2022-10-23 13:07:45 +0530

    import java.util.Scanner; import java.util.Arrays; class Unique {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,j,c=0,max=-1,k=0;         n=sc.nextInt();         int x[]=new int[n];         int y[]=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             k=0;             k=x[i]*x[i];             y[i]=k;         }         Arrays.sort(y);         for(i=0;i<n;i++)         {             System.out.print(y[i]+" ");         }     } }

---
+++ _Squares_of_a_Sorted_Array.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Unique
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,j,c=0,max=-1,k=0;
+        n=sc.nextInt();
+        int x[]=new int[n];
+        int y[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            k=0;
+            k=x[i]*x[i];
+            y[i]=k;
+        }
+        Arrays.sort(y);
+        for(i=0;i<n;i++)
+        {
+            System.out.print(y[i]+" ");
+        }
+    }
+}
